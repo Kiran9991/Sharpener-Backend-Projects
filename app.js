797f1f5ab5a0c686403b89872587eac135d9ee17
Kiran9991 <122242670+Kiran9991@users.noreply.github.com>
2023-03-04 19:51:08 +0530

    Reading data from a file

--- app.js
+++ app.js
@@ -5,14 +5,17 @@ const server = http.createServer((req, res) => {
   const url = req.url;
   const method = req.method;
   if (url === '/') {
+    fs.readFile("message.txt", {encoding:'utf-8'}, (err, data) => {
+      console.log('the data is '+data)
     res.write('<html>');
     res.write('<head><title>Enter Message</title><head>');
-    res.write(`<p>${this.message}</p>`)
+    res.write(`<p>${data}</p>`)
     res.write(
       `<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>`
     );
     res.write('</html>');
     return res.end();
+  })
   }
   if (url === '/message' && method === 'POST') {
     const body = [];
@@ -32,12 +35,12 @@ const server = http.createServer((req, res) => {
       });
     });
   }
-  res.setHeader('Content-Type', 'text/html');
-  res.write('<html>');
-  res.write('<head><title>My First Page</title><head>');
-  res.write('<body><h1>Hello from my Node.js Server!</h1></body>');
-  res.write('</html>');
-  res.end();
+  // res.setHeader('Content-Type', 'text/html');
+  // res.write('<html>');
+  // res.write('<head><title>My First Page</title><head>');
+  // res.write('<body><h1>Hello from my Node.js Server!</h1></body>');
+  // res.write('</html>');
+  // res.end();
 });
 
 server.listen(3000);
